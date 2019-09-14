2f58c5e6d7fd1c9112b5d1d3780cfc3e5529a9e5
D.IVIM <dg@ivim.ca>
2019-09-14 16:04:21 -0400

    SE added 2008

--- PSES_plotFunctions.R
+++ PSES_plotFunctions.R
@@ -1,5 +1,7 @@
-# source("PSES_plotFunctions.R")
+# source("PSES-plotFunctions.R")
 source("PSES_functions.R")
+
+library(plotly)
 
 image_smileys <- readPNG("feedback2.png")
 
@@ -13,13 +15,48 @@ if (F) {
 }
 if (F) {
   readPsesData()
-
+  testPsesData()
+  
+  if (F) { # SED data
+    
+    q="Q43"
+    for (q in dtPSES$QUESTION %>% unique) {
+      myScore <- dtPSES[as.list(myID)][QUESTION==q]$SCORE100; myScore
+      myScoreAve <- dtPSES[QUESTION==q, median(SCORE100)];  myScoreAve
+      myRank <- (dtPSES[QUESTION==q& SCORE100 > myScore, .N] / dtPSES[QUESTION==q, .N] * 100) %>% as.integer(); myRank
+      
+      
+      n <- substring(q, 2) %>% as.integer(); n
+      myScores2011 <-  dtSED[Year==2011 & n2018==n]$Score; myScores2011
+      if (F) {
+        dtQuestions[QUESTION==q, ':='(myScore=myScore)]
+      }
+    }
+  }
+  
+  # inp <- list()
+  # inp$year <- myYears
+  # inp$question <- dtQuestions[QUESTION==myQ]$Question.Abbreviated
+  
+  myID <- myID0
+  myID = getIDupto(myID0,2);myID
+  selectDepts (id=myID)
+  
   
   setkeyv(dtDepartments,PSES_ID_COLS)  
   dtRESULT <<- dtPSES[SURVEYR %in% c(2018)][
     selectDepts (id=myID), on=PSES_ID_COLS][
       dtQuestions, on="QUESTION"]
   
+  
+  # Problem here: 
+  if (dtRESULT[.N]$IDlevel == 0) {
+    dtRESULT <<-  dtRESULT[which.max(ANSCOUNT)]
+  } else {
+    dtRESULT <<-  dtRESULT [, ':='(SCORE100=median(SCORE100), ANSCOUNT=max(ANSCOUNT)) ,
+                            by=.(SURVEYR,QUESTION,Organization)] %>%
+      unique(by=c("SURVEYR","QUESTION","Organization"))
+  }
   
   ############################################################## #
   # TEST:  Depts_vs_Scores ------------------------------ 
@@ -35,7 +72,7 @@ if (F) {
   
   # dtRESULT <<- dtRESULT[IDlevel <=  getLevel(id)+1]
   
-  Depts_vs_Scores(input)
+  Depts_vs_Scores(inp)
   
   ############################################################## #
   # TEST:  Questions_vs_Scores ----  
@@ -51,12 +88,8 @@ if (F) {
 }
 
 
-plotPSES.layout <- function (input) {
-  ggplot(dtRESULT
-         # dtRESULT[SURVEYR==input$year],
-         # dtRESULT[IDlevel <=  getLevel(myID)+1],
-         )  + 
-    theme_bw() +
+plotPSES.layout <- function () {
+  ggplot()  + theme_bw() +
     # geom_text(aes(x,y, label=":("), data=data.table(x=40,y=ySmiley/2),size=6) +
     # geom_text(aes(x,y, label=":)"), data=data.table(x=60,y=ySmiley/2),size=6)  + 
     scale_x_continuous(breaks=(3:9)*10, limit=c(30,90)) +
@@ -71,29 +104,35 @@ plotPSES.layout <- function (input) {
 
 
 ################################################################### #
-Depts_vs_Scores <-  function(input)  {
+Depts_vs_Scores <-  function(inp="name")  {
   
-
+  if (inp$sortby =="name"){
+    dtRESULT[ , Organization := reorder(Organization, Organization)]
+    dtRESULT[ , Organization := reorder(Organization, IDlevel)]
+  } else if (inp$sortby =="number of responses"){
+    dtRESULT[ , Organization := reorder(Organization, ANSCOUNT)]
+    dtRESULT[ , Organization := reorder(Organization, IDlevel)] 
+  } else { # (inp$sortby =="score"){
+    dtRESULT[ , Organization := reorder(Organization , -SCORE100) ]
+    dtRESULT[ , Organization := reorder(Organization , IDlevel)]
+  } 
+  
   
   if (T) {
-    g <- plotPSES.layout(input) + 
-
+    g <- plotPSES.layout() + 
       annotation_raster(image_smileys, xmin = 46, xmax = 54, ymin = 0.2, ymax = 1.8) +
-      geom_vline(aes(xintercept=dtRESULT[SURVEYR==input$year & IDlevel==0]$SCORE100), linetype=4,  col="black") +
+      geom_vline(aes(xintercept=dtRESULT[IDlevel==0]$SCORE100), linetype=4,  col="black") +
       #guides(colour = guide_legend("Public Service average")) +
       
-      # geom_point(aes(SCORE100, Organization, col=SURVEYR),size=10, alpha=0.2) +
-      geom_label(aes(SCORE100, Organization, 
-                     label=ANSCOUNT,
-                    fill=SURVEYR
-                    ), alpha=0.2) +
-      ##guides(fill = guide_legend("Theme")) + 
-      ## scale_fill_brewer(palette = "Blues")  
-      # guides(size="none") +
-      # guides(colour="none") +
-      # guides(fill="none")
-    guides(fill= guide_legend("SURVEY YEAR")) + 
-      theme(legend.position = "bottom")   
+      # geom_point(aes(SCORE100, Organization,  col=as.ordered(level)),size=10) +
+      geom_label(data=dtRESULT[IDlevel <=  getLevel(myID)+1],
+                 mapping=aes(SCORE100, Organization, label=ANSCOUNT, fill=as.ordered(IDlevel)) ) +
+      #guides(fill = guide_legend("Theme")) + 
+      # scale_fill_brewer(palette = "Blues")  
+      # facet_grid(level ~ .)
+      guides(size="none") +
+      guides(colour="none") +
+      guides(fill="none") 
   }
   
   g
@@ -102,24 +141,27 @@ Depts_vs_Scores <-  function(input)  {
 
 
 ################################################################### #
-Questions_vs_Scores <- function(input)  {
+Questions_vs_Scores <- function(inp="name")  {
   # if (dtRESULT %>% nrow == 0)
   #   return (NULL)
-
+  
+  if (F){ # was crashing here: Warning: Error in : $ operator is invalid for atomic vectors
+    dtRESULT[ , Question.Abbreviated := as.character(Question.Abbreviated)]
     
-    if (input$sortby =="name"){
+    if (inp$sortby =="name"){
       dtRESULT[ , Question.Abbreviated := reorder(Question.Abbreviated , nTheme )] 
-    } else if (input$sortby =="number of responses"){
+    } else if (inp$sortby =="number of responses"){
       dtRESULT[ , Question.Abbreviated := reorder(Question.Abbreviated, ANSCOUNT)]
-    } else if (input$sortby =="score"){
+    } else if (inp$sortby =="score"){
       dtRESULT[ , Question.Abbreviated := reorder(Question.Abbreviated , -SCORE100) ]
     } else {
-      dtRESULT[ , Question.Abbreviated := reorder(Question.Abbreviated , RANK100) ]
+      dtRESULT[ , Question.Abbreviated := reorder(Question.Abbreviated , RANK0) ]
       
     }
     
- # dtRESULT[ , Question.Abbreviated := as.character(Question.Abbreviated)]
- # dtRESULT[ , Question.Abbreviated := reorder(Question.Abbreviated , -nTheme )] 
+  }
+  # dtRESULT[ , Question.Abbreviated := as.character(Question.Abbreviated)]
+  # dtRESULT[ , Question.Abbreviated := reorder(Question.Abbreviated , -nTheme )] 
   
   # if (sortbyscore){
   #   dtRESULT[ , Question.Abbreviated := reorder(Question.Abbreviated , -SCORE100) ]
@@ -127,7 +169,7 @@ Questions_vs_Scores <- function(input)  {
   #   #   dtRESULT[ , Question.Abbreviated := as.ordered(Question.Abbreviated )]
   #   dtRESULT[ , Question.Abbreviated := reorder(Question.Abbreviated , nTheme )] 
   # }
-
+  
   
   # Show only for parents, not children
   #dtRESULT <<- dtRESULT[level <=  myLevel(id)]
@@ -141,7 +183,7 @@ Questions_vs_Scores <- function(input)  {
     
     geom_point(data=dtRESULT[IDlevel==getLevel(myID)],mapping=aes(SCORE100, Question.Abbreviated, col=as.ordered(Theme)) , size=7, alpha=0.8) +
     
-    geom_label(data=dtRESULT[IDlevel==getLevel(myID)],mapping=aes(SCORE100, Question.Abbreviated, label=RANK100) ,  alpha=0.4) +
+    geom_label(data=dtRESULT[IDlevel==getLevel(myID)],mapping=aes(SCORE100, Question.Abbreviated, label=RANK0) ,  alpha=0.4) +
     
     geom_point(data=dtRESULT[IDlevel==0],mapping=aes(SCORE100, Question.Abbreviated), 
                size=7,  shape=3, col="black") + # shape=15)
@@ -153,8 +195,6 @@ Questions_vs_Scores <- function(input)  {
   #   geom_point(data=dtRESULT[level==myLevel(id)+2],mapping=aes(SCORE100, Question.Abbreviated, col=as.ordered(Theme)), size=1, alpha=0.6) 
   #   
   #   
-  
-  dtRESULT[ , Question.Abbreviated := as.character(Question.Abbreviated)]
   
   
   g
@@ -192,7 +232,7 @@ if(F) { # extra left -----
   
   
   
-  #input$theme
+  #inp$theme
   psesSED.plotThemeByTime_facets <- function(.dt, .strTheme) {
     
     
@@ -245,25 +285,3 @@ if(F) { # extra left -----
   
 }
 
-
-
-# ALSO SEE examples: ------
-# 
-# 
-if (F) {
-#https://sebastiansauer.github.io/plotting_surveys/
-ggplot(dtRESULT, aes(x = items)) +
-  geom_bar(aes(fill = answer), position = "fill") 
-  
-}
-
-
-if (F) {
-#https://rmarkdown.rstudio.com/flexdashboard/examples.html
-# highcharter
-#
-# https://beta.rstudioconnect.com/jjallaire/htmlwidgets-highcharter/htmlwidgets-highcharter.html
-# also different title bar:
-# http://jkunst.com/flexdashboard-highcharter-examples/pokemon/
-# 
-}
